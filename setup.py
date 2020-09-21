17ebb4960219645c5db4c9b57a48ceb28e0c863d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-21 13:21:31 +0100

    Added setup.py v0.0.1

---
+++ setup.py
@@ -0,0 +1,54 @@
+# Classification (U)
+
+"""Program:  setup.py
+
+    Description:  A setuptools based setup module.
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import os
+import setuptools
+
+# Third-party
+
+# Local
+import version
+
+
+# Read in long description from README file.
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md")) as f_hdlr:
+    LONG_DESCRIPTION = f_hdlr.read()
+
+setuptools.setup(
+    name="RabbitMQ_Metadata",
+    description="RabbitMQ program to process PDF files.",
+    author="Mark Pernot",
+    author_email="Mark.J.Pernot@coe.ic.gov",
+    url="https://sc.appdev.proj.coe.ic.gov/JAC-DSXD/rmq-metadata",
+    version=version.__version__,
+    platforms=["Linux"],
+    long_description=LONG_DESCRIPTION,
+
+    classifiers=[
+        # Common Values:
+        #  1 - Pre-Alpha
+        #  2 - Alpha
+        #  3 - Beta
+        #  4 - Field
+        #  5 - Production/Stable
+        "Development Status :: 1 - Pre-Alpha",
+        "Operating System :: Linux",
+        "Operating System :: Linux :: Centos",
+        "Operating System :: Linux :: Ubuntu",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Database",
+        "Topic :: Database :: RabbitMQ",
+        "Topic :: Database :: RabbitMQ :: 3.6.6",
+        "Topic :: Database :: Mongodb",
+        "Topic :: Database :: Mongodb :: 3.4.2"])
