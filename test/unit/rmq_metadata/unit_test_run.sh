8ce3867509b62f64f00262b9e0b4621d023665dc
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-21 14:24:59 +0100

    Added test/unit/rmq_metadata/unit_test_run.sh v0.0.1

---
+++ test/unit/rmq_metadata/unit_test_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Unit testing program for the rmq_metadata.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit test:  rmq_metadata.py"
+test/unit/rmq_metadata/_convert_data.py
+test/unit/rmq_metadata/_process_queue.py
+test/unit/rmq_metadata/help_message.py
+test/unit/rmq_metadata/validate_create_settings.py
+test/unit/rmq_metadata/non_proc_msg.py
+test/unit/rmq_metadata/process_msg.py
+test/unit/rmq_metadata/monitor_queue.py
+test/unit/rmq_metadata/run_program.py
+test/unit/rmq_metadata/main.py
+
