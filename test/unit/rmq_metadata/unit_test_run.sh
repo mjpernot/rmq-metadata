de84d927d1921237a313034e5211129434e0f750
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-24 13:25:52 +0100

    Updated test/unit/rmq_metadata/unit_test_run.sh v0.0.1

--- test/unit/rmq_metadata/unit_test_run.sh
+++ test/unit/rmq_metadata/unit_test_run.sh
@@ -9,6 +9,7 @@ echo "Unit test:  rmq_metadata.py"
 test/unit/rmq_metadata/_convert_data.py
 test/unit/rmq_metadata/_process_queue.py
 test/unit/rmq_metadata/_sort_data.py
+test/unit/rmq_metadata/create_metadata.py
 test/unit/rmq_metadata/find_tokens.py
 test/unit/rmq_metadata/get_pypdf2_data.py
 test/unit/rmq_metadata/help_message.py
