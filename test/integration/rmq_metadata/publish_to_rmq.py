cf0ad089f70145e96f6702d7d02bb68b4af06c7f
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-10-06 11:03:07 +0100

    Added test/integration/rmq_metadata/publish_to_rmq.py v0.1.0

---
+++ test/integration/rmq_metadata/publish_to_rmq.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  publish_to_rmq.py
+
+    Description:  Publish a PDF file to RabbitMQ in integration testing.
+
+    Usage:
+        test/integration/rmq_metadata/publish_to_rmq.py
+
+    Arguments:
+        filename -> Path and file name to an email with a PDF file attached.
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import os
+import sys
+import datetime
+
+# Third-party
+import base64
+import email.Parser
+
+# Local
+sys.path.append(os.getcwd())
+import lib.gen_libs as gen_libs
+import rabbit_lib.rabbitmq_class as rabbitmq_class
+
+
+def parse_email_file(fname):
+
+    """Function:  parse_email_file
+
+    Description:  Open and parse an email from a file.
+
+    Arguments:
+        (input) fname -> Name of email file.
+        (outout) msg -> Email parser instance.
+
+    """
+
+    parser = email.Parser.Parser()
+    msg = parser.parse(open(fname))
+
+    return msg
+
+
+def publish_msg(rmq, fname):
+
+    """Function:  publish_msg
+
+    Description:  Open an encoded PDF file and publish it to RabbitMQ.
+
+    Arguments:
+        (input) rmq -> RabbitMQ instance.
+        (input) fname -> File name of encoded PDF file.
+        (output) status -> True|False - Status of publication to RabbitMQ.
+        (output) err_msg -> Any error messages encountered.
+
+    """
+
+    status = True
+    err_msg = None
+
+    with open(fname, "r") as f_hldr:
+        body = f_hldr.read()
+
+    if not rmq.publish_msg(body):
+        err_msg = "\tError:  Failed to publish message to RabbitMQ."
+        status = False
+        archive_email(rmq, body)
+
+    return status, err_msg
+
+
+def create_rq_pub(cfg):
+
+    """Function:  create_rq_pub
+
+    Description:  Create and return a RabbitMQ Publication instance.
+
+    Arguments:
+        (input) cfg -> Configuration module name.
+        (output) rmq -> RabbitMQ class instance.
+
+    """
+
+    rmq = rabbitmq_class.RabbitMQPub(
+        cfg.user, cfg.japd, cfg.host, cfg.port,
+        exchange_name=cfg.exchange_name, exchange_type=cfg.exchange_type,
+        queue_name=cfg.queue_list[0]["queue"],
+        routing_key=cfg.queue_list[0]["routing_key"],
+        x_durable=cfg.x_durable, q_durable=cfg.q_durable,
+        auto_delete=cfg.auto_delete)
+    connect_status, err_msg = rmq.create_connection()
+
+    if connect_status and rmq.channel.is_open:
+        return rmq
+
+    else:
+        print("Error:  Failed to connect to RabbitMQ as Publisher.")
+        print("Error Message: %s" % (err_msg))
+
+        return None
+
+
+def run_program(fname):
+
+    """Function:  run_program
+
+    Description:  Determine whether to process the file as a PDF file or as an
+        email with a PDF attachment.  Then base64 encode the PDF to a file,
+        load a configuration file, and then publish the PDF to a RabbitMQ
+        queue.
+
+    Arguments:
+        (input) fname -> Path and file name of email file with PDF attached.
+
+    """
+
+    integration_dir = "test/integration/rmq_metadata"
+    base_path = os.path.join(os.getcwd(), integration_dir)
+    tmp_dir = os.path.join(base_path, "tmp")
+    config_dir = os.path.join(base_path, "config")
+
+    _, f_ext = os.path.splitext(fname)
+
+    if f_ext == ".pdf":
+        filename = fname
+
+    # Assume an email for any other extension.
+    else:
+        msg = parse_email_file(fname)
+
+        if msg.is_multipart():
+            for item in msg.walk():
+                if item.get_content_type() in ["application/pdf"]:
+                    filename = os.path.join(tmp_dir, item.get_filename())
+                    open(filename, "wb").write(item.get_payload(decode=True))
+                    break
+
+    base64_file = filename + ".encoded"
+    base64.encode(open(filename, "rb"), open(base64_file, "wb"))
+    cfg = gen_libs.load_module("rabbitmq", config_dir)
+    rmq = create_rq_pub(cfg)
+
+    if rmq:
+        status, err_msg = publish_msg(rmq, base64_file)
+
+        if not status:
+            print("Error Message:  %s" % (err_msg))
+            print("Error:  Publish failed\n")
+
+    else:
+        print("Error:  Failed to create RabbitMQ Publisher instance")
+
+    os.remove(base64_file)
+
+
+def main():
+
+    """Function:  main
+
+    Description:  Initializes program-wide variables and processes command
+        line arguments and values.
+
+    Arguments:
+        (input) sys.argv -> Arguments from the command line.
+        (input) **kwargs:
+            argv_list -> List of arguments from another program.
+
+    """
+
+    cmdline = gen_libs.get_inst(sys)
+    fname = cmdline.argv[1]
+    run_program(fname)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
