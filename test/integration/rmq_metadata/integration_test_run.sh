d37b8d29c883cf27f063bb6724e0fa90e038c01c
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-10-06 14:55:03 +0100

    Added test/integration/rmq_metadata/integration_test_run.sh v0.1.0

---
+++ test/integration/rmq_metadata/integration_test_run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Integration testing program for the rmq_metadata.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Integration test:  rmq_metadata.py"
+#test/integration/rmq_metadata/_convert_data.py
+#test/integration/rmq_metadata/_process_queue.py
+#test/integration/rmq_metadata/_sort_data.py
+test/integration/rmq_metadata/_validate_files.py
+#test/integration/rmq_metadata/create_metadata.py
+#test/integration/rmq_metadata/extract_pdf.py
+#test/integration/rmq_metadata/find_tokens.py
+#test/integration/rmq_metadata/get_pdfminer_data.py
+#test/integration/rmq_metadata/get_pypdf2_data.py
+#test/integration/rmq_metadata/get_textract_data.py
+#test/integration/rmq_metadata/main.py
+#test/integration/rmq_metadata/merge_data.py
+#test/integration/rmq_metadata/monitor_queue.py
+#test/integration/rmq_metadata/non_proc_msg.py
+#test/integration/rmq_metadata/pdf_to_string.py
+#test/integration/rmq_metadata/process_msg.py
+#test/integration/rmq_metadata/read_pdf.py
+#test/integration/rmq_metadata/run_program.py
+#test/integration/rmq_metadata/summarize_data.py
+test/integration/rmq_metadata/validate_create_settings.py
+
