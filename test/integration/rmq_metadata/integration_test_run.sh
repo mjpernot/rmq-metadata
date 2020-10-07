24f873e5acd5ad519de20d0986538cc171d93078
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-10-07 09:12:01 +0100

    Updated test/integration/rmq_metadata/integration_test_run.sh v0.1.0

--- test/integration/rmq_metadata/integration_test_run.sh
+++ test/integration/rmq_metadata/integration_test_run.sh
@@ -12,7 +12,7 @@ echo "Integration test:  rmq_metadata.py"
 test/integration/rmq_metadata/_validate_files.py
 #test/integration/rmq_metadata/create_metadata.py
 #test/integration/rmq_metadata/extract_pdf.py
-#test/integration/rmq_metadata/find_tokens.py
+test/integration/rmq_metadata/find_tokens.py
 #test/integration/rmq_metadata/get_pdfminer_data.py
 #test/integration/rmq_metadata/get_pypdf2_data.py
 #test/integration/rmq_metadata/get_textract_data.py
