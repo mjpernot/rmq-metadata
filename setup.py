54d6a552de1666da411564a1a50f66abc5048366
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-03-16 14:14:08 +0000

    Updated setup.py v0.1.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
         #  3 - Beta
         #  4 - Field
         #  5 - Production/Stable
-        "Development Status :: 2 - Alpha",
+        "Development Status :: 3 - Beta",
         "Operating System :: Linux",
         "Operating System :: Linux :: Centos",
         "Operating System :: Linux :: Ubuntu",
