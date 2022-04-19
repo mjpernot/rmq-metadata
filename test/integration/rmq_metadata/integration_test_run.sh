863b1169d74774ff05143ef55a821f5afc56792d
Mark Pernot <mark.pernot@gmail.com>
2022-04-19 12:06:21 +0100

    Update integration test run files v0.1.0

--- test/integration/rmq_metadata/integration_test_run.sh
+++ test/integration/rmq_metadata/integration_test_run.sh
@@ -6,10 +6,7 @@
 
 echo ""
 echo "Integration test:  rmq_metadata.py"
-test/integration/rmq_metadata/_convert_data.py
-test/integration/rmq_metadata/_process_queue.py
-test/integration/rmq_metadata/_sort_data.py
-test/integration/rmq_metadata/_validate_files.py
+test/integration/rmq_metadata/convert_data.py
 test/integration/rmq_metadata/extract_pdf.py
 test/integration/rmq_metadata/find_tokens.py
 test/integration/rmq_metadata/get_pdfminer_data.py
@@ -18,9 +15,12 @@ test/integration/rmq_metadata/get_textract_data.py
 test/integration/rmq_metadata/main.py
 test/integration/rmq_metadata/non_proc_msg.py
 test/integration/rmq_metadata/pdf_to_string.py
+test/integration/rmq_metadata/process_message.py
 test/integration/rmq_metadata/process_msg.py
 test/integration/rmq_metadata/read_pdf.py
 test/integration/rmq_metadata/run_program.py
+test/integration/rmq_metadata/sort_data.py
 test/integration/rmq_metadata/summarize_data.py
 test/integration/rmq_metadata/validate_create_settings.py
+test/integration/rmq_metadata/validate_files.py
 
